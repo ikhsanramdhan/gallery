3a62d82e56f6256297b7361d20054695d1b4b814
afiw1 <razxyy99@gmail.com>
2024-03-20 07:49:12 +0700

    progres pertama

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex justlify-content-between">
+                        <div>images</div>
+                    <div>
+                        <form class="form-inline">
+                            <select class="form-control">
+                                <option>Oldest<option>
+                                <option>Latest<option>
+                            </select> 
+                    </div>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
